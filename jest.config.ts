81bc26115d1f7645e3f54b605d1aaf053773a993
Tientcheu Israel <israel01tientcheu@gmail.com>
2026-05-05 22:37:21 +0100

    chore(caisse): configure jest-preset-angular for unit tests

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', 'src/app/app\\.spec\\.ts'],
+};
+
+export default config;
